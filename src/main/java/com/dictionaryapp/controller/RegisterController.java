06439eda221052de0de29d6325c75e73156d5ec6
Стефан Христов <133797718+StefanHristov1997@users.noreply.github.com>
2024-06-17 15:25:51 +0300

    Add register page

--- src/main/java/com/dictionaryapp/controller/RegisterController.java
+++ src/main/java/com/dictionaryapp/controller/RegisterController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
-public class LoginController {
+public class RegisterController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
-
 }
